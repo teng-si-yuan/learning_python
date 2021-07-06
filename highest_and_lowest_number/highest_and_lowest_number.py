386b509729adf38255be7699f6c35187fa4e5c71
Teng_Siyuan <tengsiyuan@gmail.com>
2021-07-06 18:07:28 +0800

    rename_function_parameter

--- highest_and_lowest_number/highest_and_lowest_number.py
+++ highest_and_lowest_number/highest_and_lowest_number.py
@@ -1,17 +1,17 @@
-def highest_and_lowest_number(yangmiemie):
+def highest_and_lowest_number(number_array):
 	y = 0
 	z = 100000000
-	if len(yangmiemie) == 0:
+	if len(number_array) == 0:
 		return
-	if len(yangmiemie) == 1:
-		yangmiemie[0]
-		return(yangmiemie[0], yangmiemie[0])
-	for x in yangmiemie:
+	if len(number_array) == 1:
+		number_array[0]
+		return(number_array[0], number_array[0])
+	for x in number_array:
 		if y < x:
 			y = x
 		if z > x:
 			z = x
 	return(y, z)
 
-yangmiemie = [2, 4]
-print(highest_and_lowest_number(yangmiemie))
+number_array = [2, 4]
+print(highest_and_lowest_number(number_array))

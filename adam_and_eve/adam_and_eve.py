fdf80f43f10b79534b33c69bd12daf7fc86daf5c
Teng_Siyuan <tengsiyuan@gmail.com>
2021-07-16 16:54:26 +0800

    add subclass (man and woman)

--- adam_and_eve/adam_and_eve.py
+++ adam_and_eve/adam_and_eve.py
@@ -3,10 +3,19 @@ class Human:
 		self.name = name
 		self.sex = sex
 
+class Man(Human):
+	def __init__(self, name):
+		Human.__init__(self, name, 'male')
+
+class Woman(Human):
+	def __init__(self, name):
+		Human.__init__(self, name, 'female')
+
 def create_humans():
 	first_humans = []
-	adam = Human('adam', 'male')
-	eve = Human('eve', 'female')
+	adam = Man('adam')
+	eve = Woman('eve')
 	first_humans.append(adam)
 	first_humans.append(eve)
 	return(first_humans)
+
--- adam_and_eve/test_adam_and_eve.py
+++ adam_and_eve/test_adam_and_eve.py
@@ -3,11 +3,17 @@ from adam_and_eve import create_humans
 
 class TestAdamAndEve(unittest.TestCase):
 
-	def test_adam(self):
+	def test_adam1(self):
 		self.assertEqual(create_humans()[0].name, 'adam')
 
-	def test_eve(self):
+	def test_eve1(self):
 		self.assertEqual(create_humans()[1].name, 'eve')
+
+	def test_adam2(self):
+		self.assertEqual(create_humans()[0].sex, 'male')
+
+	def test_eve2(self):
+		self.assertEqual(create_humans()[1].sex, 'female')
 
 if __name__ == '__main__':
 	unittest.main()

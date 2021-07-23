701ca365aab97de71b155f2f2b6759b342bf50c7
Teng_Siyuan <tengsiyuan@gmail.com>
2021-07-23 14:18:35 +0800

    add negative number test to sort

--- sort/test_sort.py
+++ sort/test_sort.py
@@ -15,6 +15,9 @@ class TestSort(unittest.TestCase):
 	def test_normal_five_numbers(self):
 		self.assertEqual(sort([1, 3, 2, 7, 6]), [1, 2, 3, 6, 7])
 
+	def test_negative_number(self):
+		self.assertEqual(sort([-1, -3, 2, 7, -6]), [-6, -3, -1, 2, 7])
+
 
 if __name__ == '__main__':
 	unittest.main()

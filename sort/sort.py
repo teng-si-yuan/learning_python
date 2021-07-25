82b751901b9258ad98191821ffbe9115c0b6225d
Teng_Siyuan <tengsiyuan@gmail.com>
2021-07-25 14:13:19 +0800

    support sort in ascending and descending order

--- sort/sort.py
+++ sort/sort.py
@@ -8,12 +8,27 @@ def lowest_number(number_array):
 		if z > x:
 			z = x
 	return(z)
-def sort(unordered_array):
+
+def highest_number(number_array):
+	y = -10000000
+	if len(number_array) == 0:
+		return([])
+	if len(number_array) == 1:
+		return(number_array[0])
+	for x in number_array:
+		if y < x:
+			y = x
+	return(y)
+
+def sort(unordered_array, ascending):
 	result = []
 	if len(unordered_array) == 0 or len(unordered_array) == 1:
 		return(unordered_array)
 	while len(unordered_array) >= 1:
-		x = lowest_number(unordered_array)
-		result.append(x)
+		if ascending == True:
+			x = lowest_number(unordered_array)
+		if ascending == False:
+			x = highest_number(unordered_array)
+		result.append(x)					
 		unordered_array.remove(x)
 	return(result)
--- sort/test_sort.py
+++ sort/test_sort.py
@@ -3,20 +3,29 @@ from sort import sort
 
 class TestSort(unittest.TestCase):
 
-	def test_none(self):
-		self.assertEqual(sort([]), [])
+	def test_ascending_none(self):
+		self.assertEqual(sort([], True), [])
 
-	def test_only_one(self):
-		self.assertEqual(sort([1]), [1])
+	def test_ascending_only_one(self):
+		self.assertEqual(sort([1], True), [1])
 
-	def test_normal_two_numbers(self):
-		self.assertEqual(sort([2, 1]), [1, 2])
+	def test_ascending_normal_two_numbers(self):
+		self.assertEqual(sort([2, 1], True), [1, 2])
 
-	def test_normal_five_numbers(self):
-		self.assertEqual(sort([1, 3, 2, 7, 6]), [1, 2, 3, 6, 7])
+	def test_ascending_normal_five_numbers(self):
+		self.assertEqual(sort([1, 3, 2, 7, 6], True), [1, 2, 3, 6, 7])
 
-	def test_negative_number(self):
-		self.assertEqual(sort([-1, -3, 2, 7, -6]), [-6, -3, -1, 2, 7])
+	def test_ascending_negative_number(self):
+		self.assertEqual(sort([-1, -3, 2, 7, -6], True), [-6, -3, -1, 2, 7])
+
+	def test_descending_normal_two_numbers(self):
+		self.assertEqual(sort([2, 1], False), [2, 1])
+
+	def test_descending_normal_five_numbers(self):
+		self.assertEqual(sort([1, 3, 2, 7, 6], False), [7, 6, 3, 2, 1])
+
+	def test_descending_negative_five_numbers(self):
+		self.assertEqual(sort([-1, -3, 2, 7, -6], False), [7, 2, -1, -3, -6])
 
 
 if __name__ == '__main__':

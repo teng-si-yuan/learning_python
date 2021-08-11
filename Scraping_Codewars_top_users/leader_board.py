d7b73767bd130ebe6c106215162604482ead2cc3
Teng_Siyuan <tengsiyuan@gmail.com>
2021-08-11 16:46:32 +0800

    able to parse name, rank, clan, honor from tr

--- Scraping_Codewars_top_users/leader_board.py
+++ Scraping_Codewars_top_users/leader_board.py
@@ -6,14 +6,24 @@ class LeaderBoard:
 
 	def scrape(self):
 		with urllib.request.urlopen('https://www.codewars.com/users/leaderboard/') as response:
-   			html = response.read()
-   			soup = BeautifulSoup(html, 'html.parser')
-   			rows = soup.find_all('tr')
-   			# cells = rows[1].children[0].text
-   			print(rows[1].get('data-username'))
-   			print(rows[1])
-   			for child in rows[1].children:
-   				print(child.text)
+			html = response.read()
+			soup = BeautifulSoup(html, 'html.parser')
+			rows = soup.find_all('tr')
+			# cells = rows[1].children[0].text
+			name = rows[1].get('data-username')
+			for child in rows[1].children:
+				if child.text.startswith("#"):
+					rank = child.text
+					continue
+				if child.text.count(",") > 0:
+					honor = child.text
+					continue
+				if child.text.count("kyu") == 0 and child.text.count("dan") == 0:
+					clan = child.text
+			print(name)
+			print(rank)
+			print(honor)
+			print(clan)
  
 leader_board = LeaderBoard()
 leader_board.scrape()
